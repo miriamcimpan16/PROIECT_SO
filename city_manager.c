4559779d7df260cb8921109429aadd9bb3d69a12
miriamcimpan16 <miricimpan@gmail.com>
2026-04-17 21:13:45 +0300

    Creez si celelalte doua fisiere

--- city_manager.c
+++ city_manager.c
@@ -19,16 +19,43 @@ void creare_fisiere(char district[])
 	//district/reports.dat
 	//district/district.cfg
 	//district/logged_district
-	char path_reports[MAX];
+      //CREARE FISIERULUI REPORTS.DAT
+      char path_reports[MAX];
       snprintf(path_reports,MAX,"%s/reports.dat",district);
       int fd = open(path_reports,O_CREAT | O_RDWR,0664);
+      //O_CREAT-> spune daca fisierul exista sau nu, daca nu exista este create
+      //O_RDWR->deschide fisierul pentru citire si pentru scriere
       if(fd == -1)
 	{
 	  printf("eroare la creare reports.dat");
 	  exit(EXIT_FAILURE);
 	}
       close(fd);
-      chmod(path_reports,0664);
+      chmod(path_reports,0664); // pun permsiunile inca odata, desi le am pus la open, pt sigurata,deoarece pot fi modificate de catre sistem
+      //CREAREA FISIERULUI DISTRICT.CFG
+      char path_cfg[MAX];
+      snprintf(path_cfg, MAX, "%s/district.cfg", district); 
+      fd = open(path_cfg, O_CREAT | O_RDWR, 0640);
+      if(fd == -1) {
+	perror("eroare la creare district.cfg"); 
+	exit(EXIT_FAILURE);
+      }
+      close(fd);
+      chmod(path_cfg, 0640);
+   
+      //CREAREA FISIERULUI LOGGED_DISTRICT
+      char path_log[MAX];
+      snprintf(path_log, MAX, "%s/logged_district", district);
+      fd = open(path_log, O_CREAT | O_RDWR, 0644);
+        if (fd == -1)
+        {
+            perror("eroare la creare logged_district");
+            exit(EXIT_FAILURE);
+        }
+        close(fd);
+        chmod(path_log, 0644);
+      
+      
       
     }
   else
